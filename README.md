53acd68b65b5056933e7c22ee9b416e8cd5ed267
xwolf <2324808561@qq.com>
2018-02-02 14:26:00 +0800

    数据源

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 >* Hibernate validator 集成
 >* jackson 日期时间格式化
 >* swagger2 api管理
+
+启动成功后,
+访问[http://localhost:8082/swagger-ui.html](http://localhost:8082/swagger-ui.html) 
+即可查看swagger接口文档信息.
--- src/main/java/com/xwolf/boot/config/SwaggerConfig.java
+++ src/main/java/com/xwolf/boot/config/SwaggerConfig.java
@@ -35,8 +35,7 @@ public class SwaggerConfig {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Spring Boot中使用Swagger2构建RESTful APIs")
-                .description("更多Spring Boot相关文章请关注：http://blog.didispace.com/")
-                .termsOfServiceUrl("http://blog.didispace.com/")
+                .description("Swagger API 接口文档")
                 .contact("程序猿")
                 .version("1.0")
                 .build();
